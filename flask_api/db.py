551e14f62002bf87228ce4343bf112012fd18992
crystal song <csong67@yahoo.com>
2021-03-10 00:50:50 -0500

    basic setup of flask api

---
+++ flask_api/db.py
@@ -0,0 +1,11 @@
+"""
+This file will manage interactions with our data store.
+At first, it will just contain stubs that return fake data
+Gradually, we will fill in actuall calls to our datastore.
+"""
+
+def fetch_locations():
+    """
+    A function to return all locations
+    """
+    return {"Bob's Burgers": 2, "Joe's Soup Kitchen": 1, "Cat Shelter": 2}
---
+++ flask_api/endpoints.py
@@ -0,0 +1,49 @@
+"""
+This is the file containing all the endpoints for our flask app.
+The endpoint called 'endpoints' will return all available endpoints.
+"""
+
+from flask_api import FlaskAPI
+from flask_restx import Resource, Api
+from db import fetch_locations
+
+app = FlaskAPI(__name__)
+api = Api(app)
+
+
+@api.route('/')
+class Home(Resource): 
+    """
+    The purpose of the Home class is to test if 
+    the app is working. 
+    """
+    def get(self):
+        """
+        A trivial endpoint to see if the server is running.
+        Returns "success"
+        """
+        return {'success'}
+
+@api.route('/endpoints')
+class Endpoints(Resource):
+    """
+    This class serves as a live, fetchable documentation of what endpoints
+    are available in the system.
+    """
+    def get(self):
+        """
+        the get() method will return a list of available endpoints.
+        """
+        endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
+        return {"Available endpoints": endpoints}
+
+@api.route('/locations')
+class Locations(Resource):
+    """
+    This class supports fetching a list of locations
+    """
+    def get(self):
+        """
+        This method returns all locations.
+        """
+        return fetch_locations()
