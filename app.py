8eb2143c31312824777fbf1c7f97b7d9cf307a59
Tanya Jain <tanyajain0018@gmail.com>
2021-04-03 19:04:17 -0400

    flask pages for donate and explore

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = IMAGES_FOLDER
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index.html")
+@app.route("/donate.html")
+@app.route("/explore.html")
+
 def show_index():
 	logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
 	background = os.path.join(app.config['UPLOAD_FOLDER'], 'background.png')
