c88c06cc56dd57cbe6a0d79e15fe6442b8eab820
Tanya Jain <tanyajain0018@gmail.com>
2021-04-14 02:31:46 -0400

    Added login form and fuctionality

--- source/app.py
+++ source/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import os
+#from flask_sqlalchemy import SQLAlchemy
+
+#db = SQLAlchemy()
 
 IMAGES_FOLDER = os.path.join('static', 'images')
 
@@ -7,19 +10,27 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = IMAGES_FOLDER
 
 @app.route("/")
-@app.route("/index.html")
-@app.route("/exlpore.html")
+@app.route("/index.html", methods=["POST", "GET"])
 def show_index():
-	logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
-	background = os.path.join(app.config['UPLOAD_FOLDER'], 'background.png')
-	return render_template('index.html', logo=logo, background=background)
+    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
+    background = os.path.join(app.config['UPLOAD_FOLDER'], 'background.png')
+    if request.method == "POST":
+        user = request.form["uname"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template('index.html', logo=logo, background=background)
 
-#@app.route("/explore.html")
+@app.route("/explore.html")
 def show_explore():
-	pass
+    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
+    return render_template('explore.html', logo=logo)
 
-#@app.route("/profile.html")
-def show_profile():
-    pass
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>Welcome {usr}</h1>"
+    
+#@app.route("/login")
+#def login():
+#   return render_template
 
 app.run(debug = True)
