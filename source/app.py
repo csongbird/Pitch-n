c4020853e3af1d9c106b4da45d1bc8b9d526587d
Avik Gomes <aag668@nyu.edu>
2021-04-14 00:20:00 -0400

    Updated app.py

--- source/app.py
+++ source/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import os
 
 IMAGES_FOLDER = os.path.join('static', 'images')
@@ -6,27 +6,20 @@ IMAGES_FOLDER = os.path.join('static', 'images')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = IMAGES_FOLDER
 
-
 @app.route("/")
 @app.route("/index.html")
+@app.route("/exlpore.html")
 def show_index():
-    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
-    background = os.path.join(app.config['UPLOAD_FOLDER'], 'background.png')
-    return render_template('index.html', logo=logo, background=background)
+	logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
+	background = os.path.join(app.config['UPLOAD_FOLDER'], 'background.png')
+	return render_template('index.html', logo=logo, background=background)
 
-
-@app.route("/donate.html")
-def show_donate():
-    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
-    background = os.path.join(app.config['UPLOAD_FOLDER'], 'background.png')
-    return render_template('donate.html', logo=logo, background=background)
-
-
-@app.route("/explore.html")
+#@app.route("/explore.html")
 def show_explore():
-    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
-    background = os.path.join(app.config['UPLOAD_FOLDER'], 'background.png')
-    return render_template('explore.html', logo=logo, background=background)
+	pass
 
+#@app.route("/profile.html")
+def show_profile():
+    pass
 
-app.run(debug=True)
+app.run(debug = True)
