90f2cf23afc0a7d5915fff82dec98754db144729
crystal song <csong67@yahoo.com>
2021-04-17 20:16:16 -0400

    fixed flake8 errors

--- source/app.py
+++ source/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, render_template, redirect, url_for
 import os
-#from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-#db = SQLAlchemy()
+# db = SQLAlchemy()
 
 IMAGES_FOLDER = os.path.join('static', 'images')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = IMAGES_FOLDER
+
 
 @app.route("/")
 @app.route("/index.html", methods=["POST", "GET"])
@@ -20,17 +21,20 @@ def show_index():
     else:
         return render_template('index.html', logo=logo, background=background)
 
+
 @app.route("/explore.html")
 def show_explore():
     logo = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
     return render_template('explore.html', logo=logo)
 
+
 @app.route("/<usr>")
 def user(usr):
     return f"<h1>Welcome {usr}</h1>"
-    
-#@app.route("/login")
-#def login():
+
+# @app.route("/login")
+# def login():
 #   return render_template
 
-app.run(debug = True)
+
+app.run(debug=True)
