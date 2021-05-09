3622650c0ba94770d99aac2cb2c69163289e666d
Avik Gomes <aag668@nyu.edu>
2021-05-09 19:21:05 -0400

    Added Donation Centers to Map

--- source/maps.py
+++ source/maps.py
@@ -3,10 +3,27 @@ from folium import Map, Marker, Icon
 
 def generate_map():
     map = Map(location=[40.694481, -73.98666], zoom_start=15)
-
-    popup = '<A href="http://www.google.com" target="_blank">Click Me!</A>'
+    popup = '<A href="/center1.html">Callahan\'s Castaways</A>'
     map.add_child(
         Marker(location=[40.694481, -73.98666],
+               popup=popup,
+               icon=Icon(color='purple')))
+
+    popup = '<A href="/center2.html">Tanya\'s Terrific Givers</A>'
+    map.add_child(
+        Marker(location=[40.69936, -73.97898],
+               popup=popup,
+               icon=Icon(color='purple')))
+
+    popup = '<A href="/center3.html">Avik\'s Able Homes</A>'
+    map.add_child(
+        Marker(location=[40.69032, -73.999357],
+               popup=popup,
+               icon=Icon(color='purple')))
+
+    popup = '<A href="/center4.html">Crystal Clear Cause</A>'
+    map.add_child(
+        Marker(location=[40.68579, -73.98040],
                popup=popup,
                icon=Icon(color='purple')))
 
