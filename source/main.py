d2c0733dfb4cf39b78b0375089b0d29ba61f65d4
Tanya Jain <tanyajain0018@gmail.com>
2021-05-09 21:09:44 -0400

    Added dashboards for centers

--- source/main.py
+++ source/main.py
@@ -7,12 +7,19 @@ import os
 main = Blueprint('main', __name__)
 IMAGES_FOLDER = os.path.join('static', 'images')
 flask_app.config['UPLOAD_FOLDER'] = IMAGES_FOLDER
+furniture = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'furniture.png')
+clothing = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'clothes.png')
+housewares = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'pan.png')
+electronics = os.path.join(flask_app.config['UPLOAD_FOLDER'],
+                           'electronics.png')
+food = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'food.png')
+logo = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'logo.png')
+books = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'books.png')
 
 
 @main.route('/profile')
 @login_required
 def profile():
-    logo = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'logo.png')
     pic = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'profilePic.png')
     center1 = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'center1.png')
     center2 = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'center2.png')
@@ -28,7 +35,6 @@ def profile():
 @main.route('/organization')
 @login_required
 def organization():
-    logo = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'logo.png')
     pic = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'profilePic.png')
     org_id = request.args.get('org_id')
     current_user = Organization.get_org_with_id(org_id)
@@ -36,3 +42,52 @@ def organization():
                            name=current_user.username,
                            logo=logo,
                            pic=pic)
+
+
+# Code below is hard-coded demo code used for presentation
+@main.route('/center1.html')
+def center1():
+    pic = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'center1.png')
+    return render_template('center1.html', logo=logo,
+                           pic=pic,
+                           name="Callahan's Castaways",
+                           clothing=clothing,
+                           furniture=furniture,
+                           housewares=housewares)
+
+
+@main.route('/center2.html')
+def center2():
+    pic = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'center2.png')
+    return render_template('center2.html', logo=logo,
+                           pic=pic,
+                           name="Tanya's Terrific Givers",
+                           clothing=clothing,
+                           furniture=furniture,
+                           housewares=housewares,
+                           food=food, books=books,
+                           electronics=electronics,
+                           )
+
+
+@main.route('/center3.html')
+def center3():
+    pic = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'center3.png')
+    return render_template('center3.html', logo=logo,
+                           pic=pic,
+                           name="Avik's Able Homes",
+                           clothing=clothing,
+                           furniture=furniture, books=books,
+                           housewares=housewares)
+
+
+# Code below is hard-coded demo code used for presentation
+@main.route('/center4.html')
+def center4():
+    pic = os.path.join(flask_app.config['UPLOAD_FOLDER'], 'center4.png')
+    return render_template('center4.html', logo=logo,
+                           pic=pic,
+                           name="Crystal Clear Cause",
+                           clothing=clothing,
+                           furniture=furniture, food=food,
+                           electronics=electronics)
